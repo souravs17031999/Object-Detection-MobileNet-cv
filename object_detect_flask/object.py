267f820c459497e69e14d1bcebf386a08839cba0
sourav kumar <souravs_1999@rediffmail.com>
2020-04-04 16:09:24 +0530

    update some useful comments

--- object_detect_flask/object.py
+++ object_detect_flask/object.py
@@ -11,33 +11,44 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 target = os.path.join(APP_ROOT, 'images/')
 full_path = target + 'result.png'
 
+# defining default route
 @app.route('/',methods = ['GET','POST'])
 def hello_world():
+	'''
+	methods :
+		GET : is used to request data from a specified resource.
+		POST : is used to send data to a server to create/update a resource.
+	'''
+	# if we are just opening the webpage
 	if request.method == 'GET':
 		return render_template('index.html',name = "sourav")
+	# if we are creating POST requests when filling the form for uploading the image
 	if request.method == 'POST':
-		print(request.files)
+		# checking for file upload errors
 		if 'file' not in request.files:
 			return render_template('error.html', message='FILE_LOAD_ERROR : File not loaded properly, please try again !')
 		file = request.files['file']
+		# checking for file upload errors  , if empty means nothing selected
 		if file.filename == '':
 		    return render_template('error.html', message='FILE_LOAD_ERROR : File not loaded properly, please try again !')
 		image = file.read()
 		npimg = np.fromstring(image, np.uint8)
+		# converting all queries from text box to lower case as we have defined all classes to be in lower case
 		query = request.form['text'].lower()
+		# check if nothing is provided in text box
 		if not query:
 			return render_template('error.html', message='EMPTY_STRING_ERROR : Query string is empty, please enter something !')
-		print(file)
+		# deleting older images to save space
 		for file in os.listdir(target):
 			os.remove(target + file)
 
+		# this is the file path of info about model file for caffe
 		prototxt = APP_ROOT + '/info.txt'
+		# this is the caffemodel containing weights of the pre trained model
 		model = APP_ROOT + '/model.caffemodel'
 		m = MobileNetSSD(npimg, prototxt, model, 0.5, query, True)
-		result_flag = m.main()
+		result_flag = m.main()  # this flag tells whether we detected given object in the image or not 
 		if result_flag[0]:
 			return render_template('result.html', image_name=result_flag[1])
 		else:
 			return render_template('error.html', message='NOT_FOUND_ERROR : Given Object not found in the Provided Image')
-
-		
